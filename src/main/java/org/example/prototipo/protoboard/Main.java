e93797879dd546921fcdbe636cf9d68676ddfbec
Krisstal <khernandez22@alumnos.utalca.cl>
2024-08-17 23:15:53 -0400

    Scene builder y movimiento.
    Creación de scene builder y sus funciones además de movimiento de elementos.

--- src/main/java/org/example/prototipo/protoboard/Main.java
+++ src/main/java/org/example/prototipo/protoboard/Main.java
@@ -2,23 +2,42 @@ package org.example.prototipo.protoboard;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import  java.io.IOException;
 import javafx.stage.Stage;
 
-import org.example.prototipo.HelloApplication;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
 
+<<<<<<< Updated upstream
 
         Prototipo_Protoboard proto = new Prototipo_Protoboard();
 
 
 
         Scene scene= new Scene(proto, 1280, 840);
+=======
+        Group grupo=new Group();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/prototipo/Fondo_builder.fxml"));
+        Parent fxmlContent = fxmlLoader.load();
+
+        Prototipo_Protoboard proto = new Prototipo_Protoboard();
+        Bateria bateria = new Bateria();
+
+
+        grupo.getChildren().addAll(proto,bateria );
+        grupo.getChildren().add(fxmlContent);
+
+
+
+        Scene scene= new Scene(grupo, 1280, 840);
+>>>>>>> Stashed changes
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Prototipo Proyecto");
