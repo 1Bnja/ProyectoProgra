a57c48388826bcc216ece931e8494d8e2fd501b8
Benjamin Navarro <bnavarro21@alumnos.utalca.cl>
2024-08-25 03:47:39 -0400

    Creación funciones standard
    Se crean nuevas funciones standard para simplificar código en las clases "Switch" y  "LED"

--- src/main/java/org/example/prototipo/protoboard/LED.java
+++ src/main/java/org/example/prototipo/protoboard/LED.java
@@ -24,10 +24,10 @@ public class LED extends Pane {
 
     public LED() {
         // LED
-        Line led1 = new Line(origenX - 550, origenY - 150, origenX - 515, origenY - 150);
-        CubicCurve curva1 = new CubicCurve(origenX - 550, origenY - 150, origenX - 549.25, origenY - 200, origenX - 515.75, origenY - 200, origenX - 515, origenY - 150);
-        pata1 = new Line(origenX - 545, origenY - 150, origenX - 545, origenY - 135);
-        pata2 = new Line(origenX - 520, origenY - 150, origenX - 520, origenY - 135);
+        Line led1 = crearLinea(origenX - 550, origenY - 150, origenX - 515, origenY - 150);
+        CubicCurve curva = crearCurva(origenX - 550, origenY - 150, origenX - 549.25, origenY - 200, origenX - 515.75, origenY - 200, origenX - 515, origenY - 150);
+        pata1 = crearLinea(origenX - 545, origenY - 150, origenX - 545, origenY - 135);
+        pata2 = crearLinea(origenX - 520, origenY - 150, origenX - 520, origenY - 135);
 
         fin1 = crearEstirable(pata1);
         fin2 = crearEstirable(pata2);
@@ -39,7 +39,41 @@ public class LED extends Pane {
         fin2.setOnMousePressed(e -> empezarArrastre(e, pata2));
         fin2.setOnMouseDragged(e -> arrastrePata(e, pata2, fin2));
 
-        // mover
+        configurarArrastre(fin1, pata1);
+        configurarArrastre(fin2, pata2);
+
+        configurarArrastreNodo();
+
+        nodo.getChildren().addAll(led1, curva, pata1, pata2, fin1, fin2);
+        this.getChildren().add(nodo);
+    }
+
+    private Line crearLinea(double startX, double startY, double endX, double endY) {
+        Line linea = new Line(startX, startY, endX, endY);
+        linea.setStroke(Color.BLACK);
+        return linea;
+    }
+
+    private CubicCurve crearCurva(double startX, double startY, double controlX1, double controlY1, double controlX2, double controlY2, double endX, double endY) {
+        CubicCurve curva = new CubicCurve(startX, startY, controlX1, controlY1, controlX2, controlY2, endX, endY);
+        curva.setStroke(Color.BLACK);
+        curva.setFill(Color.LIGHTBLUE);
+        return curva;
+    }
+
+    private void empezarArrastre(MouseEvent event, Line pata) {
+        // Indicar que estamos arrastrando una línea
+        line_en_arrastre = true;
+        mouseX = event.getSceneX();
+        mouseY = event.getSceneY();
+    }
+
+    private void configurarArrastre(Line estirable, Line pata) {
+        estirable.setOnMousePressed(e -> empezarArrastre(e, pata));
+        estirable.setOnMouseDragged(e -> arrastrePata(e, pata, estirable));
+    }
+
+    private void configurarArrastreNodo() {
         nodo.setOnMousePressed(e -> {
             if (!line_en_arrastre) {
                 mouseX = e.getSceneX();
@@ -61,22 +95,6 @@ public class LED extends Pane {
                 actualizarPosiciones();
             }
         });
-
-        led1.setStroke(Color.BLACK);
-        curva1.setStroke(Color.BLACK);
-        pata1.setStroke(Color.BLACK);
-        pata2.setStroke(Color.BLACK);
-        curva1.setFill(Color.LIGHTBLUE);
-
-        nodo.getChildren().addAll(led1, curva1, pata1, pata2, fin1, fin2);
-        this.getChildren().add(nodo);
-    }
-
-    private void empezarArrastre(MouseEvent event, Line pata) {
-        // Indicar que estamos arrastrando una línea
-        line_en_arrastre = true;
-        mouseX = event.getSceneX();
-        mouseY = event.getSceneY();
     }
 
     private void arrastrePata(MouseEvent event, Line pata, Line estirable) {
